7db577bfc53cfa4617bac7edad41f6aced92e2db
bruno-fialho <brugerf@gmail.com>
2020-10-09 16:37:18 -0300

    Add SignUp page and unform library

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,51 @@
+/* eslint-disable @typescript-eslint/ban-types */
+import React from 'react';
+import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
+
+import logoImg from '../../assets/logo.svg';
+
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+
+import { Container, Content, Background } from './styles';
+
+const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
+
+  return (
+    <Container>
+      <Background />
+
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu cadastro</h1>
+
+          <Input name="name" icon={FiUser} placeholder="Nome" />
+
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
+
+          <Button type="submit">Cadastrar</Button>
+        </Form>
+
+        <a href="asdasd">
+          <FiArrowLeft />
+          Voltar para logon
+        </a>
+      </Content>
+    </Container>
+  );
+};
+
+export default SignUp;
