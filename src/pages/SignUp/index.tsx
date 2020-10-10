648e8bec2e5486a466ab8a8c20e708eea735f8b4
bruno-fialho <brugerf@gmail.com>
2020-10-10 12:21:47 -0300

    Login validation

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -23,7 +23,7 @@ const SignUp: React.FC = () => {
       const schema = Yup.object().shape({
         name: Yup.string().required('Nome obrigatório'),
         email: Yup.string()
-          .required('E-mail obrigatóro')
+          .required('E-mail obrigatório')
           .email('Digite um e-mail válido'),
         password: Yup.string().min(6, 'No mínimo 6 dígitos'),
       });
