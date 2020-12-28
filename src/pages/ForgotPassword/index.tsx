b9cb255db379b6ccf915d4ed9d6c90564751310b
bruno-fialho <brugerf@gmail.com>
2020-12-28 14:38:44 -0300

    Implement reset password

--- src/pages/ForgotPassword/index.tsx
+++ src/pages/ForgotPassword/index.tsx
@@ -91,7 +91,7 @@ const ForgotPassword: React.FC = () => {
             </Button>
           </Form>
 
-          <Link to="/signin">
+          <Link to="/">
             <FiLogIn />
             Voltar ao login
           </Link>
