5c6deb3e8fc0979e6bec7c96036fbcb92f2689f1
bruno-fialho <brugerf@gmail.com>
2020-12-30 13:54:44 -0300

    Add month availability

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -33,6 +33,8 @@ const AuthProvider: React.FC = ({ children }) => {
     const user = localStorage.getItem('@GoBarber:user');
 
     if (token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`;
+
       return { token, user: JSON.parse(user) };
     }
 
@@ -49,6 +51,8 @@ const AuthProvider: React.FC = ({ children }) => {
 
     localStorage.setItem('@GoBarber:token', token);
     localStorage.setItem('@GoBarber:user', JSON.stringify(user));
+
+    api.defaults.headers.authorization = `Bearer ${token}`;
 
     setData({ token, user });
   }, []);
